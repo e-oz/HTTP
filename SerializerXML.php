cfa7883ece331cb53c3a6d62ecbf15a5a53b8270
Eugene OZ <normandiggs@gmail.com>
2012-04-06 03:55:47 +0400

    XML serialization and unserializaton improved

--- SerializerXML.php
+++ SerializerXML.php
@@ -10,33 +10,51 @@ class SerializerXML implements ISerializer
 	public function serialize($data)
 	{
 		if (!is_array($data)) $data = array($data);
-
 		$XML_Element = new \SimpleXMLElement("<?xml version=\"1.0\"?><".$this->xml_root_tag."></".$this->xml_root_tag.">");
-
 		$this->array_to_xml($data, $XML_Element);
-
 		return $XML_Element->asXML();
 	}
 
-	protected function array_to_xml($data_array, \SimpleXMLElement $XML_Element)
+	protected function array_to_xml($data_array, \SimpleXMLElement $XML)
 	{
+		$consecutive_counter = 0;
 		foreach ($data_array as $key => $value)
 		{
 			if (is_array($value))
 			{
-				$this->array_to_xml($value, $XML_Element->addChild($key));
-			}
-			else
-			{
-				if (is_numeric($key))
+				if (!is_numeric($key))
 				{
-					$Child = $XML_Element->addChild($this->array_item_name, $value);
-					$Child->addAttribute($this->array_item_attribute, $key);
+					$subnode = $XML->addChild($key);
+					$this->array_to_xml($value, $subnode);
 				}
 				else
 				{
-					$XML_Element->addChild($key, $value);
+					$subnode = $XML->addChild($XML->getName().'_'.$this->array_item_name);
+					if ($key!=$consecutive_counter)
+					{
+						$subnode->addAttribute($this->array_item_attribute, $key);
+					}
+					$this->array_to_xml($value, $subnode);
 				}
+			}
+			else
+			{
+				if (!is_numeric($key))
+				{
+					$XML->addAttribute($key, $value);
+				}
+				else
+				{
+					$subnode = $XML->addChild($this->array_item_name, $value);
+					if ($key!=$consecutive_counter)
+					{
+						$subnode->addAttribute($this->array_item_attribute, $key);
+					}
+				}
+			}
+			if (is_numeric($key))
+			{
+				$consecutive_counter++;
 			}
 		}
 	}
@@ -51,8 +69,72 @@ class SerializerXML implements ISerializer
 		{
 			$result = $result[$this->xml_root_tag];
 		}
-		if (key($result)==$this->array_item_name) $result = $result[$this->array_item_name];
+		$this->fix_json_xml_array($result, $this->xml_root_tag);
 		return $result;
+	}
+
+	private function fix_json_xml_array(&$xmlarr, $parent_name = '', $pre_parent_name = '')
+	{
+		foreach ($xmlarr as $key=> &$value)
+		{
+			if (is_array($value))
+			{
+				if ($key==='@attributes')
+				{
+					unset($xmlarr[$key]);
+					$xmlarr = array_merge($xmlarr, $value);
+				}
+				elseif ($key===$parent_name)
+				{
+					unset($xmlarr[$key]);
+					$xmlarr = array_merge($xmlarr, $value);
+				}
+				elseif ($key===$parent_name.'_'.$this->array_item_name)
+				{
+					unset($xmlarr[$key]);
+					$xmlarr = array_merge($xmlarr, $value);
+				}
+				elseif ($key===$pre_parent_name.'_'.$this->array_item_name)
+				{
+					unset($xmlarr[$key]);
+					$xmlarr = array_merge($xmlarr, $value);
+				}
+				elseif ($key===$this->array_item_name)
+				{
+					unset($xmlarr[$key]);
+					$xmlarr = array_merge($xmlarr, $value);
+				}
+				else
+				{
+					$this->fix_json_xml_array($value, $key, $parent_name);
+				}
+			}
+			else
+			{
+				if ($key===$parent_name)
+				{
+					unset($xmlarr[$key]);
+					$index          = $this->getNextFreeIndexOfArray($xmlarr);
+					$xmlarr[$index] = $value;
+					array_multisort($xmlarr);					
+				}
+			}
+		}
+	}
+
+	private function getNextFreeIndexOfArray($array)
+	{
+		foreach (array_keys($array) as $key)
+		{
+			if (is_numeric($key))
+			{
+				if (!isset($array[$key+1]))
+				{
+					return $key+1;
+				}
+			}
+		}
+		return 0;
 	}
 
 	/**
